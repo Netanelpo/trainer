478639754ed0d0054ad3026f45c63f858595979a
Netanel <niko0355@gmail.com>
2026-01-20 09:46:41 +0200

    netanel

--- playwright.integration.config.js
+++ playwright.integration.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@playwright/test');
 
 module.exports = defineConfig({
-    testDir: 'tests',
+    testDir: 'integration_tests',
     webServer: {
         command: 'npx serve . -l 3000',
         url: 'http://localhost:3000',
